c19005d7d31d2dd91aef77ae1c16bd33c9d9253d
mertvnl <41839620+mertvnl@users.noreply.github.com>
2020-12-14 21:13:18 +0300

    comments

--- Assets/[InterfacePrac]/Scripts/AppleController.cs
+++ Assets/[InterfacePrac]/Scripts/AppleController.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class AppleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Just a placeholder to detect the object.
 }
